0a8316061e4735185c3ee1b5a0434d25c52143e1
ArpitDCE <arpitkhatana17@gmail.com>
2026-07-15 16:15:14 +0530

    debug

--- server/index.js
+++ server/index.js
@@ -1,34 +1,36 @@
 const express = require('express');
-const axios = require('axios');
-const path = require('path');
-const cors = require('cors');
+//const axios = require('axios');
+//const path = require('path');
+//const cors = require('cors');
 
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());  
-app.use(express.json());
+//app.use(cors());  
+//app.use(express.json());
 
 // Serve static files (like index.html) from the "public" folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+//app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // API Route remains accessible
-app.get('/api/joke', async (req, res) => {
-    try {
-        const response = await axios.get('https://appspot.com');
-        res.json({ success: true, data: response.data });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
-});
+//app.get('/api/joke', async (req, res) => {
+  //  try {
+    //    const response = await axios.get('https://appspot.com');
+      //  res.json({ success: true, data: response.data });
+    //} catch (error) {
+      //  res.status(500).json({ success: false, error: error.message });
+    //}
+//});
 
 // Send React for every other route
 app.use('/',(req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.send('Hello from the server!');
+  //  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
 
