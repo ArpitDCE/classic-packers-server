500f0e7fb588f4322885f3f88ddfc6234f8234b6
ArpitDCE <arpitkhatana17@gmail.com>
2026-07-15 15:57:53 +0530

    debug

--- server/index.js
+++ server/index.js
@@ -13,9 +13,19 @@ app.use(express.json());
 // Serve static files (like index.html) from the "public" folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+// API Route remains accessible
+app.get('/api/joke', async (req, res) => {
+    try {
+        const response = await axios.get('https://appspot.com');
+        res.json({ success: true, data: response.data });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
 // Send React for every other route
 app.use('/',(req, res) => {
-    res.send("server running");
+    res.send("serrver runnig");
     //res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
